ec34520aa2861d9f01aa85251e3e6c3f8b4e4238
jfenwick <jacob.fenwick@gmail.com>
2013-10-18 14:01:44 -0400

    changed baudrate of orb to 152000. only tested on wifly, not tested on bluetooth yet

--- arduino/orb/orb.ino
+++ arduino/orb/orb.ino
@@ -164,8 +164,8 @@ void setup() {
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
-    //Serial.begin(115200);
-    Serial.begin(57600);
+    Serial.begin(115200);
+    //Serial.begin(57600);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
